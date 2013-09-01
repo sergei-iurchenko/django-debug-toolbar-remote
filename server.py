a4aed7c4c53d65e3813cc5c3c33d2c08d3172b07
hellpain <urchenko88@gmail.com>
2013-09-02 01:34:54 +0400

    correct thread termination

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
+#!/usr/bin/env python
 #coding: utf-8
 from os.path import join, normpath, dirname
 import time
-import thread
-# import threading
+import threading
 import pickle
 import logging
 import zmq
@@ -55,33 +55,44 @@ def tornado_thread():
     app.listen(8080)
 
     print('Listening on 0.0.0.0:8080')
+    global main_loop
     main_loop = tornado.ioloop.IOLoop.instance()
     main_loop.start()
 
 
-socket = None
+zeromq_socket = None
+zeromq_context = None
 
+ENABLE_ZEROMQ_LOOP = True
 
 def zeromq_thread():
-    context = zmq.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind('tcp://127.0.0.1:43000')
-    while True:
+    global zeromq_context
+    zeromq_context = zmq.Context()
+    global zeromq_socket
+    zeromq_socket = zeromq_context.socket(zmq.REP)
+    zeromq_socket.bind('tcp://127.0.0.1:43000')
+    while ENABLE_ZEROMQ_LOOP:
         try:
-            data = pickle.loads(socket.recv())
+            data = pickle.loads(zeromq_socket.recv())
             message_queue.put(data)
-            socket.send('ok')
+            zeromq_socket.send('ok')
         except Exception as e:
             print(e)
+            break
 
 message_queue = None
 
 if __name__ == '__main__':
     message_queue = Queue.Queue()
-    thread.start_new_thread(tornado_thread, ())
-    # threading.Thread(target=tornado_thread).start()
-    thread.start_new_thread(zeromq_thread, ())
-    # threading.Thread(target=zeromq_thread).start()
+    threading.Thread(target=tornado_thread).start()
+    threading.Thread(target=zeromq_thread).start()
 
     while True:
-        time.sleep(1)
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            main_loop.stop()
+            ENABLE_ZEROMQ_LOOP = False
+            zeromq_socket.close()
+            zeromq_context.term()
+            break
